612bde4455e8a69b3d06c0dbd0850916b1172595
srikarthikeyan <srikarthikeyan212001@gmail.com>
2025-09-17 09:49:48 +0530

    day1/assignment

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import React from 'react';
-
- const App = () => {
-  return <h1>Hello, World!</h1>;
-};
-
-const root = createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
