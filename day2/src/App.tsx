3b32bc57f8cecc0d6d5de1df29c46f7ed3dae670
srikarthikeyan <srikarthikeyan212001@gmail.com>
2025-09-19 09:47:07 +0530

    day2_assignment

---
+++ day2/src/App.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import Firstcomponent from './FIrstcomponent';
+import Profile from './components/Profile';
+import TodoList from './components/TodoList';
+ const App = () => {
+  return <div>
+    Hello, World!
+  <Firstcomponent />
+  
+  
+  </div>;
+};
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App />);
---
+++ day2/src/components/CompanentD.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+const ComponentD = () => {
+  return (
+    <div style={
+        {
+        "backgroundColor":"blue", 
+        "display":"fixed", 
+        "border":"2px",
+        "height":"50px",
+        "width":"100px"
+        }
+    }>ComponentD</div>
+  )
+}
+export default ComponentD;
